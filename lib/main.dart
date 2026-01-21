7eded7e288b3d9f9be52b6ead129bfd03cdf1d6f
Abd-alghfar <abdoghit12@gmail.com>
2026-01-21 12:34:32 +0200

    main.dart

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'color_provider.dart';
+import 'home_screen.dart';
+
+void main() => runApp(
+  ChangeNotifierProvider(
+    create: (context) => ColorProvider(),
+    child: const MyApp(),
+  ),
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @ksen@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const HomeScreen(),
+    );
+  }
+}
